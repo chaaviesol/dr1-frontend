a0c9743b29f00e9f5e9a03441f268991b969ae0d
Bhavanaraj12 <bhavana@chaavie.com>
2024-10-21 17:17:55 +0530

    mainadmin_onboarding_bugfix

--- src/pages/Mainadmin/Mainadminonboarding/Mainadminonboarding.jsx
+++ src/pages/Mainadmin/Mainadminonboarding/Mainadminonboarding.jsx
@@ -1,33 +1,35 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import { port } from '../../../config'
-import { useNavigate } from 'react-router-dom'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { port } from "../../../config";
+import { useNavigate } from "react-router-dom";
+import moment from "moment";
 
 export default function Mainadminonboarding() {
-  const [PendingDatas, setPendingDatas] = useState([])
-  const [initialData, setinitialData] = useState([])
+  const [PendingDatas, setPendingDatas] = useState([]);
+  const [initialData, setinitialData] = useState([]);
+  const [countdata, setcountdata] = useState([]);
   useEffect(() => {
     axios.get(`${port}/user/getunapproveuser`).then((res) => {
-      setPendingDatas(res?.data?.data)
-      setinitialData(res?.data?.data)
-    })
-  }, [])
+      setPendingDatas(res?.data?.data);
+      setinitialData(res?.data?.data);
+      setcountdata(res?.data);
+    });
+  }, []);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const SearchData = (e) => {
     const { name, value } = e?.target;
     let tempData = initialData;
-    console.log(value)
+    console.log(value);
     if (!value) {
-      setPendingDatas(PendingDatas);
+      setPendingDatas(initialData);
       return;
     }
 
-    tempData = tempData.filter(item => {
+    tempData = tempData.filter((item) => {
       let itemValue = item?.[name];
       if (itemValue !== undefined && itemValue !== null) {
-        // Convert itemValue to string for comparison
         itemValue = itemValue.toString().toLowerCase();
         return itemValue.includes(value.toLowerCase());
       }
@@ -37,24 +39,32 @@ export default function Mainadminonboarding() {
     setPendingDatas(tempData);
   };
   const linkPage = (data) => {
-    console.log("data>>>", data)
+    console.log("data>>>", data);
     if (data?.type === "Doctor") {
-      navigate("/mainadmindoctorapprove", { state: data })
+      navigate("/mainadmindoctorapprove", { state: data });
     } else if (data?.type === "Hospital") {
-      navigate("/mainadminhospitalapprove", { state: data })
-
+      navigate("/mainadminhospitalapprove", { state: data });
     } else if (data?.type === "Lab") {
-      navigate("/mainadminlabsapprove", { state: data })
-
+      navigate("/mainadminlabsapprove", { state: data });
     }
+  };
+  const reformatDate = (dateString) => {
+    return moment(dateString).format("DD-MM-YYYY");
+  };
+  const filterDate = (e) => {
+    const { value } = e.target;
 
-  }
+    const formattedDate = reformatDate(value);
+    const filteredData = initialData.filter((item) => {
+      const dateMatch = reformatDate(item.datetime) === formattedDate;
+      return dateMatch;
+    });
+    setPendingDatas(filteredData);
+  };
   return (
     <>
-
       <div className="mainadmindoctordatas_chart mainadmindoctordatas_chart_doctor flex">
-
-
+        {/* 
         <div className="mainadmindoctordatas_chart1 mainadmindoctordatas_chart10 flex">
 
           <div className="mainadmindoctordatas_chart_icon mainadmindoctordatas_chart_icon10 flex">
@@ -67,79 +77,98 @@ export default function Mainadminonboarding() {
           </div>
 
 
-        </div>
+        </div> */}
 
         <div className="mainadmindoctordatas_chart1 mainadmindoctordatas_chart2 mainadmindoctordatas_chart11  flex">
-
           <div className="mainadmindoctordatas_chart_icon mainadmindoctordatas_chart_icon11 flex">
             <i class="ri-rest-time-line"></i>
           </div>
 
           <div style={{ marginLeft: "18px" }}>
-            <h2>200</h2>
+            <h2>{countdata?.statusCounts?.P}</h2>
             <h4>Pending</h4>
           </div>
         </div>
 
         <div className="mainadmindoctordatas_chart1 mainadmindoctordatas_chart2 mainadmindoctordatas_chart12 flex">
-
           <div className="mainadmindoctordatas_chart_icon mainadmindoctordatas_chart_icon12 flex">
             <i class="ri-close-circle-line"></i>
           </div>
 
           <div style={{ marginLeft: "18px" }}>
-            <h2>200</h2>
+            <h2>{countdata?.statusCounts?.N}</h2>
             <h4>Rejected</h4>
           </div>
         </div>
-
-
       </div>
 
-
       <h3 style={{ marginBottom: "1.3vw", marginTop: "1.3vw" }}>Onboarding</h3>
-      <table className='doctortable'>
-        <tr className='doctortableTr'>
+      <table className="doctortable">
+        <tr className="doctortableTr">
           <th>No</th>
-          <th className='OnBoardingtableTh'>Type
-            <input type="text" onChange={SearchData} name='type' placeholder='Search Type' />
+          <th className="OnBoardingtableTh">
+            Type
+            <input
+              type="text"
+              onChange={SearchData}
+              name="type"
+              placeholder="Search Type"
+            />
           </th>
-          <th>Name
-            <input type="text" onChange={SearchData} name='name' placeholder='Search Name' />
-
+          <th>
+            Name <br />
+            <input
+              type="text"
+              onChange={SearchData}
+              name="name"
+              placeholder="Search Name"
+            />
           </th>
-          <th style={{ display: 'flex', flexDirection: 'column' }}>Number
-            <input type="text" onChange={SearchData} name='phone_no' placeholder='Search Number' />
-
+          <th>
+            Number <br />
+            <input
+              type="text"
+              onChange={SearchData}
+              name="phone_no"
+              placeholder="Search Number"
+            />
           </th>
-          <th>Pincode
-            <input type="text" onChange={SearchData} name='pincode' placeholder='Search Pincode' />
+          <th>
+            Pincode <br />
+            <input
+              type="text"
+              onChange={SearchData}
+              name="pincode"
+              placeholder="Search Pincode"
+            />
           </th>
-          <th>Registration Date
-            <input type="text" onChange={SearchData} name='Datetime' placeholder='Search Pincode' />
+          <th>
+            Registered Date <br />
+            <input
+              type="date"
+              max={new Date().toISOString().split("T")[0]}
+              onChange={filterDate}
+              name="Datetime"
+              placeholder={moment(new Date()).format("DD-MM-YYYY")}
+            />
           </th>
-          <th>Status
-            <input type="text" onChange={SearchData} name='status' placeholder='Search Status' />
+          <th>
+            Status
+            {/* <input type="text" onChange={SearchData} name='status' placeholder='Search Status' /> */}
           </th>
         </tr>
-        {PendingDatas.map((ele, index) =>
-
+        {PendingDatas.map((ele, index) => (
           <tr onClick={() => linkPage(ele)}>
             <td>{index + 1}</td>
             <td>{ele?.type}</td>
             <td>{ele?.name}</td>
             <td>{ele?.phone_no}</td>
             <td>{ele?.pincode}</td>
-            <td>{ele?.datetime}</td>
-            <td>{ele?.status}</td>
+            <td>{moment(ele?.datetime).format("DD-MM-YYYY")}</td>
+            <td>{ele.status && ele.status === "P" ? "Pending" : "Rejected"}</td>
           </tr>
-        )}
-
-
-
+        ))}
       </table>
-
-
     </>
-  )
+  );
 }
