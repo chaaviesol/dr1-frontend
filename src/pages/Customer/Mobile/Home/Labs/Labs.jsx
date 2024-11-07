6a2c813ea602b2ad4e6d0b667f5d300453e83d47
Bhavanaraj12 <bhavana@chaavie.com>
2024-11-07 17:24:44 +0530

    nearest_lab mobile added

--- src/pages/Customer/Mobile/Home/Labs/Labs.jsx
+++ src/pages/Customer/Mobile/Home/Labs/Labs.jsx
@@ -1,13 +1,19 @@
-import React from "react";
-import './labs.css'
+import React, { useEffect, useState } from "react";
+import "./labs.css";
 import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { BASE_URL } from "../../../../../config";
 
 function Labs() {
+  const [labdata, SetLabdata] = useState([]);
+ 
   // LabCard Component to display lab details
-  const navigate=useNavigate()
-  const LabCard = ({ image, name, location, services, rating }) => {
+  const navigate = useNavigate();
+  const LabCard = ({ image, name, location, services, rating,screen,data }) => {
     return (
-      <div className="bestlabmobcard flex">
+      <div className="bestlabmobcard flex" 
+        onClick={() => navigate(screen, { state: { data: data?.details } })}
+      >      
         <img src={image} alt={name} className="lab-image" />
         <div className="bestlabmobcardright flex">
           <h2>{name}</h2>
@@ -26,33 +32,18 @@ function Labs() {
     );
   };
 
-  // Sample data for labs
-  const labData = [
-    {
-      image:
-        "https://plus.unsplash.com/premium_photo-1663011406193-7beb9d225d31?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Micro Health Laboratories Micro Health Laboratories",
-      location: "Ernakulam, Kerala",
-      services: "ECG - USG - CT - Kidney Tests",
-      rating: 5,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1618053238059-cc7761222f2a?q=80&w=1780&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Alpha Labs llllllllllllllllllllllll uuu kpoijikjiouuuouuiuuuui",
-      location: "Cochin, Kerala",
-      services: "Blood Test - MRI - Ultrasound",
-      rating: 4,
-    },
-    {
-      image:
-        "https://images.unsplash.com/photo-1583912086201-b6db267b43ff?q=80&w=2064&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      name: "Alpha Labs",
-      location: "Cochin, Kerala",
-      services: "Blood Test - MRI - Ultrasound",
-      rating: 4,
-    },
-  ];
+  const fetch = async () => {
+    try {
+      // const pincode=====??
+      const response = await axios.post(`${BASE_URL}/lab/nearestlab`);
+      SetLabdata(response.data.data);
+    } catch (error) {
+      console.error("Error fetching lab data:", error);
+    }
+  };
+  useEffect(() => {
+    fetch();
+  }, []);
 
   return (
     <div className="avoidbottombar">
@@ -81,7 +72,7 @@ function Labs() {
             A doctor is a medical professional who diagnoses, treats, and
             prevents illnesses and injuries. They work in various{" "}
           </p>
-          <button onClick={()=>navigate("/labfiltering")}>
+          <button onClick={() => navigate("/labfiltering")}>
             Find Lab <i className="ri-arrow-right-up-line"></i>
           </button>
         </div>
@@ -92,7 +83,7 @@ function Labs() {
             A doctor is a medical professional who diagnoses, treats, and
             prevents illnesses and injuries. They work in various{" "}
           </p>
-          <button onClick={()=>navigate("/labfiltering")}>
+          <button onClick={() => navigate("/labfiltering")}>
             Find Lab <i className="ri-arrow-right-up-line"></i>
           </button>
         </div>
@@ -103,7 +94,7 @@ function Labs() {
             A doctor is a medical professional who diagnoses, treats, and
             prevents illnesses and injuries. They work in various{" "}
           </p>
-          <button onClick={()=>navigate("/labfiltering")}>
+          <button onClick={() => navigate("/labfiltering")}>
             Find Lab <i className="ri-arrow-right-up-line"></i>
           </button>
         </div>
@@ -113,7 +104,7 @@ function Labs() {
             A doctor is a medical professional who diagnoses, treats, and
             prevents illnesses and injuries. They work in various{" "}
           </p>
-          <button onClick={()=>navigate("/labfiltering")}>
+          <button onClick={() => navigate("/labfiltering")}>
             Find Lab <i className="ri-arrow-right-up-line"></i>
           </button>
         </div>
@@ -125,13 +116,19 @@ function Labs() {
         </h3>
 
         {/* Render LabCard for each lab in labData */}
-        {labData.map((lab, index) => (
+        {labdata.map((lab, index) => (
           <LabCard
+            screen="/mobilelabprofile"
             key={index}
-            image={lab.image}
+            data={{ details: lab, lab: true }}
+            image={lab?.photo?.image1}
             name={lab.name}
-            location={lab.location}
-            services={lab.services}
+            location={lab?.address}
+            services={
+              lab.services.length > 0
+                ? lab.services.join(", ")
+                : "No services available"
+            }
             rating={lab.rating}
           />
         ))}
