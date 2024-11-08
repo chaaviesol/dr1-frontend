c5b945b73aede45eeb5d593d41ba86f8adff2c4a
Chaavie Nandakishor <chaavienandakishor@gmail.com>
2024-11-08 11:12:44 +0530

    updated top hospitals section ui in mobile hospital screen

--- src/pages/Customer/Mobile/Home/Hospitals/HospitalMob.jsx
+++ src/pages/Customer/Mobile/Home/Hospitals/HospitalMob.jsx
@@ -12,13 +12,65 @@ function HospitalMob() {
 
   const { Categories } = useContext(MyContext);
   const specialities = Categories?.allopathySpecs;
+
+  const HospitalCard = ({
+    image,
+    name,
+    location,
+    services,
+    rating,
+    screen,
+    data,
+  }) => {
+    const StarRating = () => {
+      const maxRating = 5;
+      const stars = [];
+
+      for (let i = 1; i <= maxRating; i++) {
+        stars.push(
+          i <= rating ? (
+            <i
+              key={i}
+              className="ri-star-fill"
+              style={{ marginRight: "5px", fontSize: "20px", color: "#f59e0b" }}
+            />
+          ) : (
+            <i
+              key={i}
+              className="ri-star-fill"
+              style={{ color: "gray", marginRight: "5px", fontSize: "20px" }}
+            />
+          )
+        );
+      }
+
+      return <div>{stars}</div>;
+    };
+    return (
+      <div
+    
+        className="bestlabmobcard flex"
+        onClick={() => navigate(screen, { state: { data: data?.details } })}
+      >
+        <img src={image} alt={name} className="lab-image" />
+        <div className="bestlabmobcardright flex">
+          <h2>{name}</h2>
+          <div className="bestlabmobcardrightstar flex">
+            <StarRating />
+          </div>
+          <h4>{location}</h4>
+          <h3>{services}</h3>
+        </div>
+      </div>
+    );
+  };
   const handleLoadMore = () => {
     setVisibleCount((prevCount) => prevCount + 6);
   };
 
   const handleSelectSpecialization = (type, speciality) => {
     navigate("/hospitalfilter", {
-      state: { speciality: speciality, type:type },
+      state: { speciality: speciality, type: type },
     });
   };
   return (
@@ -64,7 +116,18 @@ function HospitalMob() {
       <div className={styles.margin}></div>
       <div className={styles.margin}>
         <Typography text="Top Hospitals" />
-        <TopHospitals />
+      </div>
+      <div className={styles.margin}>
+        <HospitalCard
+          screen="/hospitalfilter"
+          key={0}
+          // data={{ details: lab, lab: true }}
+          image="https://images.unsplash.com/photo-1596541223130-5d31a73fb6c6?q=80&w=1771&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          name="Abate eye hospital"
+          location="kozhikode"
+          services={"Casuality, Op"}
+          rating={2}
+        />
       </div>
     </div>
   );
