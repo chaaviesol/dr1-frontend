321cd969624f645ff164f3794cf63f0653f39691
Chaavie Nandakishor <chaavienandakishor@gmail.com>
2024-12-13 11:09:49 +0530

    port changed

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const port = "https://test.apis.dr1.co.in";
 export const BASE_URL="https://test.apis.dr1.co.in"
-export const PHARMACY_URL="http://13.232.117.141:3000"
+export const PHARMACY_URL="https://test.apis.dr1.co.in"
 // export const PHARMACY_URL="http://192.168.1.9:3000"
 
 // export const BASE_URL = "http://192.168.1.9:3003";
