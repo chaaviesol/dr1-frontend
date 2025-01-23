1439c79dccd22db463ba04d2fe493639419d8c2b
Chaavie Nandakishor <chaavienandakishor@gmail.com>
2025-01-23 09:29:08 +0530

    minor changes

--- src/config.js
+++ src/config.js
@@ -1,8 +1,8 @@
-// export const port = "https://test.apis.dr1.co.in";
-// export const BASE_URL="https://test.apis.dr1.co.in"
-// export const PHARMACY_URL="https://test.apis.dr1.co.in"
+export const port = "https://test.apis.dr1.co.in";
+export const BASE_URL="https://test.apis.dr1.co.in"
+export const PHARMACY_URL="https://test.apis.dr1.co.in"
 
-export const BASE_URL = "http://localhost:3003";
-export const port = "http://localhost:3003";
-export const PHARMACY_URL = "http://localhost:3003";
+// export const BASE_URL = "http://localhost:3003";
+// export const port = "http://localhost:3003";
+// export const PHARMACY_URL = "http://localhost:3003";
 
